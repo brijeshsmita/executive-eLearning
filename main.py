8ea1af648bebd3dba77a0136cf1ea80d01cf3382
Smita B Kumar <brijeshsmita@gmail.com>
2025-06-21 04:07:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -40,4 +40,4 @@ def index():
                            coach_response=coach_response)
 
 if __name__ == "__main__":
-    app.run(debug=True,port=5050)
+    app.run(host="0.0.0.0", port=8082, debug=True)
